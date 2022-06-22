1079a3b12e21984149c5a02dd066748899232702
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-06-22 18:16:13 +0400

    swap purple and surface

--- src/pages/one.vue
+++ src/pages/one.vue
@@ -46,8 +46,8 @@
               <div class="mt-5 flex-1 h-0 overflow-y-auto">
                 <nav class="h-full flex flex-col">
                   <div class="space-y-1">
-                    <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-purple-50 border-purple-600 text-purple-600' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group border-l-4 py-2 px-3 flex items-center text-base font-medium']" :aria-current="item.current ? 'page' : undefined">
-                      <component :is="item.icon" :class="[item.current ? 'text-purple-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-4 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
+                    <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-surface-50 border-surface-600 text-surface-600' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group border-l-4 py-2 px-3 flex items-center text-base font-medium']" :aria-current="item.current ? 'page' : undefined">
+                      <component :is="item.icon" :class="[item.current ? 'text-surface-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-4 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
                       {{ item.name }}
                     </a>
                   </div>
@@ -77,8 +77,8 @@
         </div>
         <div class="flex-grow mt-5">
           <div class="space-y-1">
-            <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-purple-50 border-purple-600 text-purple-600' : 'border-transparent text-gray-600 hover:text-gray-900 hover:bg-gray-50', 'group border-l-4 py-2 px-3 flex items-center text-sm font-medium']">
-              <component :is="item.icon" :class="[item.current ? 'text-purple-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
+            <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-surface-50 border-surface-600 text-surface-600' : 'border-transparent text-gray-600 hover:text-gray-900 hover:bg-gray-50', 'group border-l-4 py-2 px-3 flex items-center text-sm font-medium']">
+              <component :is="item.icon" :class="[item.current ? 'text-surface-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
               {{ item.name }}
             </a>
           </div>
@@ -96,7 +96,7 @@
     <div class="md:pl-64">
       <div class="max-w-4xl mx-auto flex flex-col md:px-8 xl:px-0">
         <div class="sticky top-0 z-10 flex-shrink-0 h-16 bg-white border-b border-gray-200 flex">
-          <button type="button" class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500 md:hidden" @click="sidebarOpen = true">
+          <button type="button" class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-surface-500 md:hidden" @click="sidebarOpen = true">
             <span class="sr-only">Open sidebar</span>
             <MenuAlt2Icon class="h-6 w-6" aria-hidden="true" />
           </button>
@@ -115,7 +115,7 @@
               </form>
             </div>
             <div class="ml-4 flex items-center md:ml-6">
-              <button type="button" class="bg-white rounded-full p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
+              <button type="button" class="bg-white rounded-full p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500">
                 <BellIcon class="h-6 w-6" aria-hidden="true" />
                 <span class="sr-only">View notifications</span>
               </button>
@@ -134,14 +134,14 @@
                   <!-- Tabs -->
                   <div class="lg:hidden">
                     <label for="selected-tab" class="sr-only">Select a tab</label>
-                    <select id="selected-tab" name="selected-tab" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm rounded-md">
+                    <select id="selected-tab" name="selected-tab" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-surface-500 focus:border-surface-500 sm:text-sm rounded-md">
                       <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
                     </select>
                   </div>
                   <div class="hidden lg:block">
                     <div class="border-b border-gray-200">
                       <nav class="-mb-px flex space-x-8">
-                        <a v-for="tab in tabs" :key="tab.name" :href="tab.href" :class="[tab.current ? 'border-purple-500 text-purple-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700', 'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm']">
+                        <a v-for="tab in tabs" :key="tab.name" :href="tab.href" :class="[tab.current ? 'border-surface-500 text-surface-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700', 'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm']">
                           {{ tab.name }}
                         </a>
                       </nav>
@@ -161,7 +161,7 @@
                           <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <span class="flex-grow">Chelsea Hagon</span>
                             <span class="ml-4 flex-shrink-0">
-                              <button type="button" class="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">Update</button>
+                              <button type="button" class="bg-white rounded-md font-medium text-surface-600 hover:text-surface-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500">Update</button>
                             </span>
                           </dd>
                         </div>
@@ -172,9 +172,9 @@
                               <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                             </span>
                             <span class="ml-4 flex-shrink-0 flex items-start space-x-4">
-                              <button type="button" class="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">Update</button>
+                              <button type="button" class="bg-white rounded-md font-medium text-surface-600 hover:text-surface-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500">Update</button>
                               <span class="text-gray-300" aria-hidden="true">|</span>
-                              <button type="button" class="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">Remove</button>
+                              <button type="button" class="bg-white rounded-md font-medium text-surface-600 hover:text-surface-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500">Remove</button>
                             </span>
                           </dd>
                         </div>
@@ -183,7 +183,7 @@
                           <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <span class="flex-grow">chelsea.hagon@example.com</span>
                             <span class="ml-4 flex-shrink-0">
-                              <button type="button" class="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">Update</button>
+                              <button type="button" class="bg-white rounded-md font-medium text-surface-600 hover:text-surface-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500">Update</button>
                             </span>
                           </dd>
                         </div>
@@ -192,7 +192,7 @@
                           <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <span class="flex-grow">Human Resources Manager</span>
                             <span class="ml-4 flex-shrink-0">
-                              <button type="button" class="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">Update</button>
+                              <button type="button" class="bg-white rounded-md font-medium text-surface-600 hover:text-surface-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500">Update</button>
                             </span>
                           </dd>
                         </div>
@@ -212,7 +212,7 @@
                           <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <span class="flex-grow">English</span>
                             <span class="ml-4 flex-shrink-0">
-                              <button type="button" class="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">Update</button>
+                              <button type="button" class="bg-white rounded-md font-medium text-surface-600 hover:text-surface-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500">Update</button>
                             </span>
                           </dd>
                         </div>
@@ -221,16 +221,16 @@
                           <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <span class="flex-grow">DD-MM-YYYY</span>
                             <span class="ml-4 flex-shrink-0 flex items-start space-x-4">
-                              <button type="button" class="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">Update</button>
+                              <button type="button" class="bg-white rounded-md font-medium text-surface-600 hover:text-surface-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500">Update</button>
                               <span class="text-gray-300" aria-hidden="true">|</span>
-                              <button type="button" class="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">Remove</button>
+                              <button type="button" class="bg-white rounded-md font-medium text-surface-600 hover:text-surface-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500">Remove</button>
                             </span>
                           </dd>
                         </div>
                         <SwitchGroup as="div" class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:pt-5">
                           <SwitchLabel as="dt" class="text-sm font-medium text-gray-500" passive> Automatic timezone </SwitchLabel>
                           <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            <Switch v-model="automaticTimezoneEnabled" :class="[automaticTimezoneEnabled ? 'bg-purple-600' : 'bg-gray-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:ml-auto']">
+                            <Switch v-model="automaticTimezoneEnabled" :class="[automaticTimezoneEnabled ? 'bg-surface-600' : 'bg-gray-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500 sm:ml-auto']">
                               <span aria-hidden="true" :class="[automaticTimezoneEnabled ? 'translate-x-5' : 'translate-x-0', 'inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200']" />
                             </Switch>
                           </dd>
@@ -238,7 +238,7 @@
                         <SwitchGroup as="div" class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-b sm:border-gray-200">
                           <SwitchLabel as="dt" class="text-sm font-medium text-gray-500" passive> Auto-update applicant data </SwitchLabel>
                           <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            <Switch v-model="autoUpdateApplicantDataEnabled" :class="[autoUpdateApplicantDataEnabled ? 'bg-purple-600' : 'bg-gray-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:ml-auto']">
+                            <Switch v-model="autoUpdateApplicantDataEnabled" :class="[autoUpdateApplicantDataEnabled ? 'bg-surface-600' : 'bg-gray-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-surface-500 sm:ml-auto']">
                               <span aria-hidden="true" :class="[autoUpdateApplicantDataEnabled ? 'translate-x-5' : 'translate-x-0', 'inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200']" />
                             </Switch>
                           </dd>
