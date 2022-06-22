81e960858d6e232ad97ea3f97c6e373e4885172c
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-06-22 18:14:30 +0400

    add router and page one

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import App from '@/App.vue'
+import Home from '@/pages/Home.vue'
+import One from '@/pages/one.vue'
+
+const routes = [
+  { path: "/", component: App },
+  { path: "/home", component: Home },
+  { path: "/one", component: One },
+];
+
+export default routes;
